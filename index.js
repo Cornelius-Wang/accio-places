f76a7c564ec79a0e553d727fe57e6cb7467d7992
Cory Wang <cory.wang11@gmail.com>
2020-09-03 11:12:29 -0700

    Added 2nd endpoint functionality for city property stats

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const mashvisorKey = "6875b662-8801-4be1-a329-391e54c2c507";
-const mashvisorUrl = "https://api.mashvisor.com/v1.1/client/city/properties/";
+const mashvisorUrlPlaces = "https://api.mashvisor.com/v1.1/client/city/properties/";
+const mashvisorUrlStats = "https://api.mashvisor.com/v1.1/client/city/investment/";
 const mapboxKey = "pk.eyJ1IjoiY29yeXdhbmcxMSIsImEiOiJja2RydmlhMWkwZnJxMndudXBsdGd2aDhtIn0.zlOoazi-NxpUgUbqoN_EZQ";
 const mapboxUrl = "";
 const proxyUrl = "https://ancient-inlet-96238.herokuapp.com/";
@@ -12,12 +13,12 @@ function formatQueryParams(params) {
 
 }
 
-function getPlaces(stateCode, city) {
+function getPlaces(stateCode, city, numResults) {
+
     const formCity = encodeURIComponent(city);
-    const url = mashvisorUrl + stateCode + '/' + formCity;
+    const url = mashvisorUrlPlaces + stateCode + '/' + formCity;
   
     console.log(url);
-  
     fetch(proxyUrl + url, {
       headers: {
         "x-api-key": mashvisorKey,
@@ -34,56 +35,41 @@ function getPlaces(stateCode, city) {
         $('.result-title').removeClass('hidden');
         $('.result-title').text(`Something went wrong: ${error.message}`);
       });
-  }
+}
 
-
-
-  // function getMap(x, y) {
-  //   const params = {
-  //     api_key: apiKey,
-  //     stateCode: x,
-  //     limit: y,
-  //   };
-  //   const queryString = formatQueryParams(params)
-  //   const url = getUrl + '?' + queryString;
-  
-  //   console.log(url);
-  
-  //   fetch(url)
-  //     .then(response => {
-  //       if (response.ok) {
-  //         return response.json();
-  //       }
-  //       throw new Error(response.statusText);
-  //     })
-  //     .then(responseJson => displayPlaces(responseJson))
-  //     .catch(error => {
-  //       $('.result-title').removeClass('hidden');
-  //       $('.result-title').text(`Something went wrong: ${error.message}`);
-  //     });
-  // }
-
-function templatePlaceHtml(neighborhood, placeAddress, listPrice, size, baths, beds, lastSaleDate, lastSalePrice, capRate, placeImage, mapNumber){
+function templatePlaceHtml(neighborhood, placeAddress, listPrice, size, baths, beds, capRate, placeImage, mapNumber){
 
     return `<li class="place">
     <div class="place-image"><img src="${placeImage}" class="place-photo"></div>
     <div class="place-text"><h3 class="place-title">${neighborhood}</h3>
     <p class="place-detail">${placeAddress} | ${listPrice}</p>
     <p class="place-detail"> BR ${beds} | BA ${baths} | Sq ft. ${size} | Cap Rate ${capRate}</p></div>
-    <div id="mapid-${mapNumber}"></div>
+    <div id="map-${mapNumber}"></div>
     </li>
     
     <br>`
 
 }
 
-function drawMap(latitude, longitude, number){
+// function drawPlaceMap(latitude, longitude, number){
 
-  let travereString = 'mapid-' + number.toString(); 
-  console.log(latitude,longitude,travereString);
-  L.map(travereString).setView([latitude, longitude], 13);
+//   let travereString = 'map-' + number.toString(); 
+//   console.log(latitude,longitude,travereString);
 
-}
+//   L.map(travereString, {
+//     center: [latitude, longitude],
+//     zoom: 13
+//   })
+//   L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+//     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+//     maxZoom: 18,
+//     id: 'mapbox/streets-v11',
+//     tileSize: 512,
+//     zoomOffset: -1,
+//     accessToken: mapboxKey
+//   }).addTo(travereString);
+
+// }
 
 function loopPlace(placeObject) {
 
@@ -94,7 +80,7 @@ function loopPlace(placeObject) {
 
       const currentPlace = placeObject.content.properties[i];
 
-      const html = templatePlaceHtml(
+      const placeHtml = templatePlaceHtml(
         currentPlace.neighborhood,
         currentPlace.address, 
         currentPlace.list_price_formatted, 
@@ -107,9 +93,9 @@ function loopPlace(placeObject) {
         currentPlace.image,
         i);
 
-        $('#result-list').append(html);
+        $('#result-list').append(placeHtml);
       
-      drawMap(currentPlace.latitude, currentPlace.longitude, i);
+      // drawPlaceMap(currentPlace.latitude, currentPlace.longitude, i);
 
     };
     $('.results').removeClass('hidden');
@@ -122,8 +108,78 @@ function loopPlace(placeObject) {
 function displayPlaces(responseJson) {
     /* Set array as local variable */
     const placeObject = responseJson;
-    console.log(placeObject);
     loopPlace(placeObject);
+    
+}
+
+function getStats(stateCode, city) {
+
+  const formCity = encodeURIComponent(city);
+
+  const url = mashvisorUrlStats + stateCode + '/' + formCity;
+
+  fetch(proxyUrl + url, {
+    headers: {
+      "x-api-key": mashvisorKey,
+    }
+  })
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error(response.statusText);
+    })
+    .then(responseJson => displayStats(responseJson, city))
+    .catch(error => {
+      $('.result-title').removeClass('hidden');
+      $('.result-title').text(`Something went wrong: ${error.message}`);
+    });
+}
+
+
+function templateStatsHtml(avgPrice, sqft, totalProperties, occupancy, coc, rent, city) {
+
+  return `<li class="stats">
+  <div class="stats-text"><h3 class="stats-title">${city}</h3>
+  <p class="stats-detail">Average Listing Price ${avgPrice} | Average Square Footage ${sqft} </p>
+  <p class="stats-detail"> Total properties: ${totalProperties} | Occupancy Rate ${occupancy} </p>
+  <p class="stats-detail"> Cash on Cash: ${coc} | Ave Rent Price: ${rent} </p></div>
+  </li>
+  
+  <br>`
+
+}
+
+function renderStats(statsObject, city) {
+
+  $('#stats-list').empty();
+    
+    const currentStats = statsObject.content;
+    const totalProperties = currentStats.investment_properties + currentStats.airbnb_properties + currentStats.traditional_properties;
+    console.log(totalProperties);
+
+    const statsHtml = templateStatsHtml(
+      currentStats.median_price,
+      currentStats.sqft,
+      totalProperties,
+      currentStats.occupancy,
+      currentStats.traditional_coc,
+      currentStats.traditional_rental,
+      city
+    );
+
+    $('#stats-list').append(statsHtml);
+
+  $('.stats').removeClass('hidden');
+
+}
+
+function displayStats(responseJson) {
+
+  const statsObject = responseJson;
+  console.log(statsObject);
+  renderStats(statsObject);
+
 }
 
 function formEvent() {
@@ -132,8 +188,10 @@ function formEvent() {
         event.preventDefault();
         const city = $('#city-search').val();
         const stateCode = $('#state-search').val();
+        // const numResults = $('#js-num-results').val();
         console.log(stateCode, city);
         getPlaces(stateCode, city);
+        getStats(stateCode, city);
     });
 
 }
